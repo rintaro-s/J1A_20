7178b927415ac6880099e3dfaa5150b99e8ec63c
rintaro-s@24239 <120776920+rintaro-s@users.noreply.github.com>
2024-08-01 11:53:41 +0900

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,29 +1,8 @@
-<?php
-// submit_comment.php
-$servername = "localhost";
-$username = "lorinta_comments";
-$password = "comments";
-$dbname = "lorinta_comments";
+CREATE DATABASE IF NOT EXISTS lorinta_comments;
+USE lorinta_comments;
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $comment = $_POST['comment'];
-
-    $sql = "INSERT INTO comments (username, comment) VALUES ('$username', '$comment')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "正常に送信されました！ブラウザバックしてください....";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-
-    $conn->close();
-    exit();
-}
-?>
+CREATE TABLE IF NOT EXISTS comments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    comment TEXT NOT NULL
+);
